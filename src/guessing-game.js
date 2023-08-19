30e67a1c9082c9e88f52db19777cc08be793190f
Hellty <korsovm@gmail.com>
2023-08-19 14:43:25 +0300

    feat: implement class GuessingGame

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.max=max;
+        this.min=min;
     }
 
     guess() {
-
+        this.answer = Math.round((this.max+this.min)/2);
+        return this.answer;
     }
 
     lower() {
-
+        this.max = this.answer;
     }
 
     greater() {
-
+        this.min = this.answer;
     }
 }
 
